34e4151b23e63df7e1d428b27302c9947c6e64f3
gabietaylor <gabie.taylor@yahoo.com>
2017-10-09 13:36:09 -0400

    added elsif statement and getting output

--- flow.rb
+++ flow.rb
@@ -1,11 +1,39 @@
 if 9 > 8
     puts "9 is > 8"
+elsif 
+  	puts "9 is < 8"
 else
   	puts "whatever"
 end
   
 if 9 != 8
     puts "true"
+elsif 
+  	puts "false"
 else
   	puts "haha"
 end
+
+if 1 < 2
+    puts "1 is less than 2!"
+  elsif 1 > 2
+    puts "1 is greater than 2!"
+  else
+    puts "1 equals 2!"
+  end
+
+  if 2 < 1
+    puts "2 is less than 1!"
+  elsif 2 > 1
+    puts "2 is greater than 1!"
+  else
+    puts "2 equals 1!"
+  end
+
+  if 1 < 1
+    puts "1 is less than 1!"
+  elsif 1 > 1
+    puts "1 is greater than 1!"
+  else
+    puts "1 equals 1!"
+  end
